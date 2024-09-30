e3e17439c5a5f6f9b3629a207d3b249608f397d2
JOSE CARLOS MENA DIAZ <jose.menadiaz@ucr.ac.cr>
2024-09-30 03:00:23 -0600

    Vista de developer terminada

---
+++ backend/BMT-backend/Models/DevUserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BMT_backend.Models
+{
+    public class DevUserModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Identification { get; set; }
+        public string[] AssociatedCompanies { get; set; }
+    }
+}
