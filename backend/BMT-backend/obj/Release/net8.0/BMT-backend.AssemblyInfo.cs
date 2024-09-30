e5738efe407208a42b828656b899c0e0ca5080f8
JOSE CARLOS MENA DIAZ <jose.menadiaz@ucr.ac.cr>
2024-09-30 03:16:53 -0600

    archivos faltantes 2

--- backend/BMT-backend/obj/Release/net8.0/BMT-backend.AssemblyInfo.cs
+++ backend/BMT-backend/obj/Release/net8.0/BMT-backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BMT-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3e17439c5a5f6f9b3629a207d3b249608f397d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1a86f8392b13566d4977bbacf9cd9d078e7e3b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BMT-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BMT-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
