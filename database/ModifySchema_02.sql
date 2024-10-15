7cb5c6e13dbdb47e7e4c2242da4669e9520dca45
Brandon Trigueros <brandon.trigueros@ucr.ac.cr>
2024-10-15 16:57:13 -0600

    nuevo query

--- database/ModifySchema_02.sql
+++ database/ModifySchema_02.sql
@@ -1,23 +1,22 @@
 use BMT_database;
 go 
 
--- Renombrar tabla de Product/Tags
+-- Renombrar tabla de ProductTags
 sp_rename 'Product_Tags', 'ProductTags';
 
--- Eliminar tabla actual de imágenes
+-- Eliminar tabla actual de imÃ¡genes y recrearla
 drop table Images;
 
--- Crear nueva tabla de imágenes
 create table ProductImages (
 	Id					uniqueidentifier	not null primary key default newid(),
-	ProductId				uniqueidentifier	not null foreign key references Products(Id),
+	ProductId			uniqueidentifier	not null foreign key references Products(Id),
 	URL					varchar(255)		not null unique,
 );
 
--- Añadir a usuario una columna para la dirección de la imágen de perfil
-alter table Users add ProfilePictureURL varchar(255)
+-- AÃ±adir a usuario una columna para la direcciÃ³n de la imÃ¡gen de perfil
+alter table Users add ProfilePictureURL varchar(255) not null DEFAULT 'default_url';
 
--- Modificar las restricciones de FK que referencian a products para que se haga un on delete cascade (ProductTags & ProductImages) 
+-- Modificar las restricciones de FK que referencian a products para que se haga un on delete cascade 
 DECLARE @ConstraintName NVARCHAR(200);
 DECLARE @sql NVARCHAR(MAX); 
 
@@ -29,7 +28,8 @@ IF @ConstraintName IS NOT NULL
 BEGIN
     SET @sql = N'ALTER TABLE ProductTags DROP CONSTRAINT [' + @ConstraintName + '];';
     EXEC sp_executesql @sql;
-END
+	ALTER TABLE ProductTags ADD CONSTRAINT FK_ProductTags_ProductId FOREIGN KEY (ProductId) REFERENCES Products(Id) ON DELETE CASCADE;
+END;
 
 SELECT @ConstraintName = fk.name
 FROM sys.foreign_keys fk
@@ -39,14 +39,45 @@ IF @ConstraintName IS NOT NULL
 BEGIN
     SET @sql = N'ALTER TABLE ProductImages DROP CONSTRAINT [' + @ConstraintName + '];';
     EXEC sp_executesql @sql;
-END
+	ALTER TABLE ProductImages ADD CONSTRAINT FK_ProductImages_ProductId FOREIGN KEY (ProductId) REFERENCES Products(Id) ON DELETE CASCADE;
+END;
 
--- Crear la clave foránea en ProductTags con ON DELETE CASCADE
-ALTER TABLE ProductTags
-ADD CONSTRAINT FK_ProductTags_ProductId FOREIGN KEY (ProductId)
-REFERENCES Products(Id) ON DELETE CASCADE;
+SELECT @ConstraintName = fk.name
+FROM sys.foreign_keys fk
+INNER JOIN sys.objects o ON fk.parent_object_id = o.object_id
+WHERE o.name = 'PerishableProducts' AND fk.referenced_object_id = OBJECT_ID('Products');
+IF @ConstraintName IS NOT NULL
+BEGIN
+    SET @sql = N'ALTER TABLE PerishableProducts DROP CONSTRAINT [' + @ConstraintName + '];';
+    EXEC sp_executesql @sql;
+	ALTER TABLE PerishableProducts ADD CONSTRAINT FK_PerishableProducts_ProductId FOREIGN KEY (ProductId) REFERENCES Products(Id) ON DELETE CASCADE;
+END;
 
--- Crear la clave foránea en Images con ON DELETE CASCADE
-ALTER TABLE ProductImages
-ADD CONSTRAINT FK_ProductImages_ProductId FOREIGN KEY (ProductId)
-REFERENCES Products(Id) ON DELETE CASCADE;
+SELECT @ConstraintName = fk.name
+FROM sys.foreign_keys fk
+INNER JOIN sys.objects o ON fk.parent_object_id = o.object_id
+WHERE o.name = 'NonPerishableProducts' AND fk.referenced_object_id = OBJECT_ID('Products');
+IF @ConstraintName IS NOT NULL
+BEGIN
+    SET @sql = N'ALTER TABLE NonPerishableProducts DROP CONSTRAINT [' + @ConstraintName + '];';
+    EXEC sp_executesql @sql;
+	ALTER TABLE NonPerishableProducts ADD CONSTRAINT FK_NonPerishableProducts_ProductId FOREIGN KEY (ProductId) REFERENCES Products(Id) ON DELETE CASCADE;
+END;
+
+SELECT @ConstraintName = fk.name
+FROM sys.foreign_keys fk
+INNER JOIN sys.objects o ON fk.parent_object_id = o.object_id
+WHERE o.name = 'DateDisponibility' AND fk.referenced_object_id = OBJECT_ID('Products');
+IF @ConstraintName IS NOT NULL
+BEGIN
+    SET @sql = N'ALTER TABLE DateDisponibility DROP CONSTRAINT [' + @ConstraintName + '];';
+    EXEC sp_executesql @sql;
+	ALTER TABLE DateDisponibility ADD CONSTRAINT FK_DateDisponibility_ProductId FOREIGN KEY (ProductId) REFERENCES Products(Id) ON DELETE CASCADE;
+END;
+
+-- temp querys
+--select * from Products;
+-- * from Enterprises;
+--select * from ProductImages;
+
+-- delete from Products where Name = 'Producto1'
