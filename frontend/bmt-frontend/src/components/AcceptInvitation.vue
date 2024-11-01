e3a58b9f1df6a33059412ab955fed0a52ee404fe
Alejandro Barboza Taylor <102196233+Barbo2610@users.noreply.github.com>
2024-11-01 16:09:21 -0600

    Collab register (#49)
    * Primera parte para vistas de usuarios
    
    Se implementaron en la nueva rama, una primera implementación de las vistas de usuario sin cuenta, usuario cliente y usuario emprendedor, queda pendiente su conexión con demás vistas que aún no están listas
    
    * Frontent verification
    
    * Merge Commit
    
    * Ahora se envia correos satisfactoriamente
    
    * Se completaron algunos detalles de las vistas de página principal, conexiones pendientes
    
    * Verificacion completa y actualizacion en la base de datos
    
    * revision de sesion y gitignore
    
    * archivos .vs eliminados
    
    * Primeros pasos al formulario para el registro de colaboradores
    
    * Primeros pasos al formulario para el registro de colaboradores (#11)
    
    * Eliminación de componentes por defecto
    
    * Remoción de archivos innecesarios
    
    * Conexiones realizadas a nuevas vistas y a vistas ya creadas, conexiones pendientes
    
    * Controladores para el registro de colaboradores y confirmación vía correo
    
    * Primeros pasos en registrar colaboradores, listas vistas y poco del backend
    
    * Hot fixes
    
    * Ultimos detalles, aún no está lista la user story, faltan pruebas
    
    * Faltan funcionalidades pero está un 95% listo
    
    * Refactor Vue components for readability and consistency
    
    Reformatted code in `AcceptInvitation.vue`, `CollaboratorRegisterForm.vue`, and `EnterpriseRegisterForm.vue` for better readability and consistency. Standardized import statements and refactored lifecycle methods and data properties. Added new methods for handling form submissions, invitations, and role changes. Improved template readability.
    
    * Se sigue trabajando en funcionalidades
    
    * Se añadió la confirmación con código y la busqueda por nombre de usuario
    
    * Funcionalidad completa de añadir el colaborador a una empresa
    
    * hotfix al html del correo de confirmación
    
    ---------
    
    Co-authored-by: JOSE CARLOS MENA DIAZ <jose.menadiaz@ucr.ac.cr>
    Co-authored-by: Brandon Trigueros <brandon.trigueros@ucr.ac.cr>

--- frontend/bmt-frontend/src/components/AcceptInvitation.vue
+++ frontend/bmt-frontend/src/components/AcceptInvitation.vue
@@ -1,52 +1,64 @@
 <template>
-<v-app class="d-flex flex-column">
-  <AppHeader />
-
-  <v-container v-if="loggedIn" class="d-flex justify-center align-center" style="min-height: 100vh;">
-    <v-form @submit.prevent="handleSubmit" style="width: 100%; max-width: 500px;">
-      <h1 v-if="title" class="text-center mb-5">{{ title }}</h1>
-      <v-text-field v-model="verificationCode" label="Ingresa tu código" required></v-text-field>
-      <v-btn @click="goBack" color="secondary" class="mr-2">Volver</v-btn>
-      <v-btn @click="handleSubmit" type="submit" color="primary">Verificar Código</v-btn>
-      <v-alert v-if="successMessage" type="success" class="mt-4">
-        {{ successMessage }}
-      </v-alert>
-      <v-alert v-if="errorMessage" type="error" class="mt-4">
-        {{ errorMessage }}
-      </v-alert>
-    </v-form>
-  </v-container>
-
-  <v-container v-if="enterpriseExist" class="d-flex justify-center align-center" style="min-height: 100vh;">
-    <v-card class="pa-5">
-      <v-card-title>
-        <h2 class="text-center">{{ enterprise.name }}</h2>
-      </v-card-title>
-      <v-card-text>
-        <p><strong>Nombre de la Empresa:</strong> {{ enterprise.name || 'No disponible' }}</p>
-        <p><strong>Descripción:</strong> {{ enterprise.description || 'No disponible' }}</p>
-        <v-container>
-          <v-form v-if='!isEntrepeneur' ref="form" v-model="valid" lazy-validation>
-            <v-text-field v-model="identification" label="Cédula" maxlength="9" pattern="\d{9}" required :rules="[
+	<v-app class="d-flex flex-column">
+		<AppHeader />
+    <v-container v-if = "loggedIn" class="d-flex justify-center align-center" style="min-height: 100vh;">
+      <v-form @submit.prevent="handleSubmit" style="width: 100%; max-width: 500px;">
+        <h1 v-if="title" class="text-center mb-5">{{ title }}</h1>
+        <v-text-field
+          v-model="enterpriseCode"
+          label="Ingresa el código de la empresa"
+          required
+        ></v-text-field>
+        <v-text-field
+          v-model="verificationCode"
+          label="Ingresa tu código de seguridad"
+          required
+        ></v-text-field>
+        <v-btn @click="goBack" color="secondary" class="mr-2">Volver</v-btn>
+        <v-btn @click="handleSubmit" type="submit" color="primary">Verificar Códigos</v-btn>
+        <v-alert v-if="successMessage" type="success" class="mt-4">
+          {{ successMessage }}
+        </v-alert>
+        <v-alert v-if="errorMessage" type="error" class="mt-4">
+          {{ errorMessage }}
+        </v-alert>
+      </v-form>
+    </v-container>
+    <v-container v-if="enterpriseExist" class="d-flex justify-center align-center" style="min-height: 100vh;">
+      <v-card class="pa-5">
+        <v-card-title>
+          <h2 class="text-center">{{ enterprise.name }}</h2>
+        </v-card-title>
+        <v-card-text>
+          <p><strong>Nombre de la Empresa:</strong> {{ enterprise.name || 'No disponible' }}</p>
+          <p><strong>Descripción:</strong> {{ enterprise.description || 'No disponible' }}</p>
+          <v-container>
+          <v-form v-if = "!isEntrepeneur" ref="form" v-model="valid" lazy-validation>
+            <v-text-field
+              v-model="user.Identification"
+              label="Cédula"
+              maxlength="9"
+              pattern="\d{9}"
+              required
+              :rules="[
                 v => !!v || 'Requerido',
                 v => (v.length === 9) || 'La cédula debe contener exactamente 9 dígitos',
                 v => /^\d+$/.test(v) || 'Solo se permiten dígitos'
-              ]" @keydown="handleInput"></v-text-field>
+              ]"
+              @keydown="handleInput"
+            ></v-text-field>
           </v-form>
-        </v-container>
-      </v-card-text>
-      <v-row class="mt-4" justify="space-between">
-        <v-col cols="auto">
-          <v-btn color="red" @click="declineInvitation">Declinar Invitación</v-btn>
-        </v-col>
-        <v-col cols="auto">
-          <v-btn color="green" @click="acceptInvitation">Aceptar Invitación</v-btn>
-        </v-col>
-      </v-row>
-    </v-card>
   </v-container>
-  <AppFooter />
-</v-app>
+        </v-card-text>
+        <v-row class="mt-4" justify="space-between">
+          <v-col cols="auto">
+            <v-btn color="green" @click="acceptInvitation">Aceptar Invitación</v-btn>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-container>
+    <AppFooter />
+  </v-app>
 </template>
 
 <script>
@@ -66,6 +78,7 @@ export default {
     } else {
       this.loggedIn = true;
       this.user = JSON.parse(localStorage.getItem('user')) || this.user;
+      this.user.userId = this.user.id;
     }
     if (this.user.role === 'emp') {
       this.isEntrepeneur = true;
@@ -83,6 +96,7 @@ export default {
       loggedIn: false,
       title: 'Verificación de Código',
       verificationCode: '',
+      enterpriseCode: '',
       successMessage: '',
       errorMessage: '',
       enterpriseExist: false,
@@ -106,11 +120,17 @@ export default {
     async handleSubmit() {
       try {
         const token = getToken();
-        const enterpriseResponse = await axios.get(API_URL + `/Enterprise/${this.verificationCode}`, {
+        const enterpriseResponse = await axios.get(API_URL + `/Enterprise/${this.enterpriseCode}`, {
           headers: {
             Authorization: `Bearer ${token}`
           }
         });
+
+        const codeTaken = {
+          Code: this.verificationCode,
+          Id: this.user.userId
+        };
+        await axios.post(API_URL + '/Email/verifycode', codeTaken);
 
         this.enterprise = enterpriseResponse.data;
         if (this.enterprise) {
@@ -140,7 +160,7 @@ export default {
           await this.registerEntrepreneur();
         }
 
-        const obtainEntrepreneurResponse = await axios.post(API_URL + '/Entrepreneur/GetEntrepreneurByUserId?id=' + this.user.id);
+        const obtainEntrepreneurResponse = await axios.post(API_URL + '/Entrepreneur/GetEntrepreneurByUserId?id=' + this.user.userId);
         this.entrepreneur = obtainEntrepreneurResponse.data;
 
         await this.addEntrepreneurToEnterprise(this.entrepreneur.identification, this.enterprise.identificationNumber);
@@ -161,7 +181,15 @@ export default {
           confirmButtonText: 'Ok'
         });
       }
+      this.sendConfirmationMail();
       this.goBack();
+    },
+
+    async sendConfirmationMail(){
+      await axios.post(API_URL + '/Email/sendconfirmationmail', {
+        Email: this.enterprise.email,
+        Id: this.user.username
+      });
     },
 
     async registerEntrepreneur() {
@@ -205,10 +233,6 @@ export default {
           confirmButtonText: 'Ok'
         });
       }
-    },
-
-    async declineInvitation() {
-      this.goBack();
     },
 
     goBack() {
