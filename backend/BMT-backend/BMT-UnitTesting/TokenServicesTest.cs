6e9d71983e5e880f4f90da3d87222752b759127a
Alejandro Barboza Taylor <102196233+Barbo2610@users.noreply.github.com>
2024-11-01 19:35:50 -0600

    Creación del Proyecto de Unit Testing

---
+++ backend/BMT-backend/BMT-UnitTesting/TokenServicesTest.cs
@@ -0,0 +1,16 @@
+namespace BMT_UnitTesting
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
