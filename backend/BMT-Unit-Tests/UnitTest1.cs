d9b5df82b57e3e81800ba8e0eabacc90c46a9e82
Jose Mena <163797497+J053e@users.noreply.github.com>
2024-11-18 20:37:26 -0600

    Creado proyecto de tests funcional nuevo

---
+++ backend/BMT-Unit-Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace BMT_Unit_Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
