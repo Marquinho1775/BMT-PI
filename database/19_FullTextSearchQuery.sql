cf5938f173161230606e432e39f66bfccf162876
Brandon Trigueros <brandon.trigueros@ucr.ac.cr>
2024-11-26 00:52:32 -0600

    Se utiliza la base de datos del profesor, corrección de queries

--- database/19_FullTextSearchQuery.sql
+++ database/19_FullTextSearchQuery.sql
@@ -1,17 +1,86 @@
+-- 1. Verificar y crear el catálogo de texto completo si no existe
 IF NOT EXISTS (SELECT * FROM sys.fulltext_catalogs WHERE name = 'FTCatalog')
-CREATE FULLTEXT CATALOG FTCatalog AS DEFAULT;
+BEGIN
+    CREATE FULLTEXT CATALOG FTCatalog AS DEFAULT;
+    PRINT 'Catálogo de texto completo "FTCatalog" creado.'
+END
+ELSE
+BEGIN
+    PRINT 'Catálogo de texto completo "FTCatalog" ya existe.'
+END
 GO
 
-create fulltext index on Products(Name, Description)
-key index PK__Products__3214EC07905BE42C on FTCatalog;
-go
+-- 2. Función para obtener el nombre del índice de clave primaria de una tabla
+CREATE OR ALTER FUNCTION dbo.GetPrimaryKeyIndexName(@TableSchema SYSNAME, @TableName SYSNAME)
+RETURNS SYSNAME
+AS
+BEGIN
+    DECLARE @IndexName SYSNAME;
 
-create fulltext index on Enterprises(Name, Description)
-key index PK__Enterpri__3214EC0791052781 on FTCatalog;
-go
+    SELECT @IndexName = i.name
+    FROM sys.indexes i
+    INNER JOIN sys.key_constraints kc
+        ON i.object_id = kc.parent_object_id
+        AND i.index_id = kc.unique_index_id
+    WHERE kc.parent_object_id = OBJECT_ID(QUOTENAME(@TableSchema) + '.' + QUOTENAME(@TableName))
+        AND kc.type = 'PK';
 
+    RETURN @IndexName;
+END
+GO
 
--- SELECT p.Id, Result.RANK as Relevance
--- FROM Products p INNER JOIN CONTAINSTABLE(Products, (Name, Description), 'isabout("Galletas", navidad weight(1), padre weight(.5))') as Result
--- ON p.Id = Result.[Key]
--- ORDER BY Result.RANK DESC;
+-- 3. Procedimiento para crear índices de texto completo dinámicamente
+CREATE OR ALTER PROCEDURE dbo.CreateFullTextIndex
+    @SchemaName SYSNAME,
+    @TableName SYSNAME,
+    @Columns NVARCHAR(MAX)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @IndexName SYSNAME;
+    DECLARE @SQL NVARCHAR(MAX);
+
+    -- Obtener el nombre del índice de clave primaria
+    SET @IndexName = dbo.GetPrimaryKeyIndexName(@SchemaName, @TableName);
+
+    IF @IndexName IS NULL
+    BEGIN
+        PRINT 'No se encontró un índice de clave primaria para la tabla ' + QUOTENAME(@SchemaName) + '.' + QUOTENAME(@TableName) + '.';
+        RETURN;
+    END
+
+    -- Construir la sentencia SQL para crear el índice de texto completo
+    SET @SQL = '
+        IF NOT EXISTS (
+            SELECT * FROM sys.fulltext_indexes fi
+            INNER JOIN sys.objects o ON fi.object_id = o.object_id
+            WHERE o.name = ''' + @TableName + ''' AND SCHEMA_NAME(o.schema_id) = ''' + @SchemaName + '''
+        )
+        BEGIN
+            CREATE FULLTEXT INDEX ON ' + QUOTENAME(@SchemaName) + '.' + QUOTENAME(@TableName) + '(' + @Columns + ')
+            KEY INDEX ' + QUOTENAME(@IndexName) + ' ON FTCatalog;
+            PRINT ''Índice de texto completo creado para ' + @SchemaName + '.' + @TableName + ''' 
+        END
+        ELSE
+        BEGIN
+            PRINT ''El índice de texto completo ya existe para ' + @SchemaName + '.' + @TableName + ''' 
+        END
+    ';
+
+    -- Ejecutar la sentencia SQL dinámica
+    EXEC sp_executesql @SQL;
+END
+GO
+
+-- 4. Ejecutar el procedimiento para las tablas "Products" y "Enterprises"
+EXEC dbo.CreateFullTextIndex @SchemaName = 'dbo', @TableName = 'Products', @Columns = 'Name, Description';
+GO
+
+EXEC dbo.CreateFullTextIndex @SchemaName = 'dbo', @TableName = 'Enterprises', @Columns = 'Name, Description';
+GO
+
+-- 5. Limpiar objetos creados (opcional)
+-- DROP PROCEDURE dbo.CreateFullTextIndex;
+-- DROP FUNCTION dbo.GetPrimaryKeyIndexName;
+-- DROP FULLTEXT CATALOG FTCatalog;
